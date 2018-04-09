77d67c981afc779c1e353f7541aac6e0c05f6c2f
Loic Laurent <loic.lau@gmail.com>
2018-04-09 11:35:59 +0200

    la descente

---
+++ laDescente/laDescente.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * The while loop represents the game.
+ * Each iteration represents a turn of the game
+ * where you are given inputs (the heights of the mountains)
+ * and where you have to print an output (the index of the mountain to fire on)
+ * The inputs you are given are automatically updated according to your last actions.
+ **/
+
+// game loop
+while (true) {
+    $mountains = array();
+
+    for ($i = 0; $i < 8; $i++) {
+        fscanf(STDIN, "%d",
+            $mountainH // represents the height of one mountain.
+        );
+
+        $mountains[$i] = $mountainH;
+        arsort($mountains);
+    }
+
+    $highestMountain = array_keys($mountains)[0];
+
+    // Write an action using echo(). DON'T FORGET THE TRAILING \n
+    // To debug (equivalent to var_dump): error_log(var_export($var, true));
+
+    echo("$highestMountain\n");
+}
+?>
